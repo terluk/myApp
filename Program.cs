78ac8f90f71f71c361f580e0a110193e121e0ec7
Dzmitry <dzmitry@MacBook-Air-Dzmitry.local>
2021-10-07 23:13:56 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");
+            Console.WriteLine("Current time is " + DateTime.Now);*/
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();       // вводим имя
+            Console.WriteLine($"Привет {name} " + "\n" + DateTime.Now);
+            /* Dzima -> myApp mlmlmlmlmlm ADCSDCwfwN*/
+        }
+    }
+}
